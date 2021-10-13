7e5bab9cda94c0d2c2256ff836c7020700cabb6d
Jobin Johnson <jobinjohnson01334@gmail.com>
2021-10-13 21:23:15 +0530

    Movies and Series Hub Project

---
+++ src/components/Genres.js
@@ -0,0 +1,72 @@
+import { Chip } from '@material-ui/core';
+import axios from 'axios';
+import React, { useEffect } from 'react';
+
+const Genres = ({
+  selectedGenres,
+  setSelectedGenres,
+  genres,
+  setGenres,
+  setPage,
+  type,
+}) => {
+  const handleAdd = (genre) => {
+    setSelectedGenres([...selectedGenres, genre]);
+    setGenres(genres.filter((g) => g.id !== genre.id));
+    setPage(1);
+  };
+
+  const handleRemove = (genre) => {
+    setSelectedGenres(
+      selectedGenres.filter((selected) => selected.id !== genre.id)
+    );
+    setGenres([...genres, genre]);
+    setPage(1);
+  };
+
+  const fetchGenres = async () => {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/genre/${type}/list?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
+    );
+
+    setGenres(data.genres);
+  };
+
+  useEffect(() => {
+    fetchGenres();
+
+    return () => {
+      setGenres({});
+    }; // eslint-disable-next-line
+  }, []);
+
+  return (
+    <div style={{ padding: '6px 0' }}>
+      {selectedGenres &&
+        selectedGenres.map((genre) => (
+          <Chip
+            label={genre.name}
+            key={genre.id}
+            style={{ margin: 2 }}
+            size='small'
+            color='primary'
+            clickable
+            onDelete={() => handleRemove(genre)}
+          />
+        ))}
+      {genres &&
+        genres.map((genre) => (
+          <Chip
+            label={genre.name}
+            key={genre.id}
+            style={{ margin: 2 }}
+            size='small'
+            clickable
+            onClick={() => handleAdd(genre)}
+          />
+        ))}
+    </div>
+  );
+};
+
+export default Genres;
---
+++ src/components/Pagination/CustomPagination.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core';
+import Pagination from '@material-ui/lab/Pagination';
+
+const darkTheme = createMuiTheme({
+  palette: {
+    type: 'dark',
+  },
+});
+
+const CustomPagination = ({ setPage, numOfPages = 10 }) => {
+  const handlePageChange = (Page) => {
+    setPage(Page);
+    window.scroll(0, 0);
+  };
+
+  return (
+    <div
+      style={{
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        marginTop: 10,
+      }}
+    >
+      <ThemeProvider theme={darkTheme}>
+        <Pagination
+          count={numOfPages}
+          onChange={(e) => handlePageChange(e.target.textContent)}
+          color='primary'
+        />
+      </ThemeProvider>
+    </div>
+  );
+};
+
+export default CustomPagination;
